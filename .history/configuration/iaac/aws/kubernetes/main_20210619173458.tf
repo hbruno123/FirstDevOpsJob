91050d572b0ebc4e6b6af6fda15e4efd35cff217
harold_jbc@hotmail.com <harold_jbc@hotmail.com>
2021-06-19 17:59:54 -0500

    subiendo cambios

--- .history/configuration/iaac/aws/kubernetes/main_20210619173458.tf
+++ .history/configuration/iaac/aws/kubernetes/main_20210619173458.tf
@@ -31,7 +31,7 @@ provider "kubernetes" {
 
 module "aforo255-cluster3" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "aforo255-cluster3"
+  cluster_name    = "aforo255-cluster2"
   cluster_version = "1.17"
   subnets         = ["subnet-2f1be252", "subnet-559fbe19"]  #CHANGE # Donot choose subnet from us-east-1e
   #subnets = data.aws_subnet_ids.subnets.ids
@@ -49,11 +49,11 @@ module "aforo255-cluster3" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.aforo255-cluster3.cluster_id
+  name = module.aforo255-cluster2.cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.aforo255-cluster3.cluster_id
+  name = module.aforo255-cluster2.cluster_id
 }
 
 
